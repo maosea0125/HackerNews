dacc6b33dc52bb777bb9c1db30598673fd227611
jmao <john.mao@expacta.com.cn>
2015-04-25 18:36:54 +0800

    translate

--- core/Common.php
+++ core/Common.php
@@ -3,48 +3,47 @@
 namespace HackerNews;
 
 class Common {
+    static function database($config) {
+        $db = new MysqliDb($config['db.host'], $config['db.username'], $config['db.password'], $config['db.database'], $config['db.port']);
+        $db->setPrefix($config['db.prefix']);
+        return $db;
+    }
 
-   static function database($config) {
-      $db = new MysqliDb($config['db.host'], $config['db.username'], $config['db.password'], $config['db.database'], $config['db.port']);
-      $db->setPrefix($config['db.prefix']);
-      return $db;
-   }
-
-   static function logincheck($redirect='') {
-      if(!isset($_SESSION['hn_login']['id'])) {
-         if($redirect) {
-            header("Location: ".$config['site_url']."login.php?redirect=".urlencode($redirect));
-         } else {
-            header("Location: ".$config['site_url']."login.php");
-         }
-         exit;
-      }
-   }
-
-   static function checkremember($config, $db) {
-
-      if(!isset($_SESSION['hn_login']['id'])) {
-         if(isset($_COOKIE[$config['cookies.name']])) {
-            $remarray = unserialize(str_replace('\\','',$_COOKIE[$config['cookies.name']]));
-
-            // Lookup the users table for that user
-            $db->where("username", validate_input($remarray['username']));
-            $db->where("user_id", validate_input($remarray['uid']));
-            $db->where("remember", validate_input($remarray['rem']));
-            $user_info = $db->get("users", 1);
-
-            // The submitted details are valid
-            if(isset($user_info[0])) {
-               $_SESSION['hn_login']['id'] = $user_info[0];
-               $_SESSION['hn_login']['name'] = $remarray['username'];
-               $_SESSION['hn_login']['comm'] = $user_info[2];
-
-               header('Location: index.php');
-               exit;
+    static function logincheck($redirect='') {
+        if(!isset($_SESSION['hn_login']['id'])) {
+            if($redirect) {
+                header("Location: ".$config['site_url']."login.php?redirect=".urlencode($redirect));
+            } else {
+                header("Location: ".$config['site_url']."login.php");
             }
-         }
-      }
-   }
+            exit;
+        }
+    }
+
+    static function checkremember($config, $db) {
+
+        if(!isset($_SESSION['hn_login']['id'])) {
+            if(isset($_COOKIE[$config['cookies.name']])) {
+                $remarray = unserialize(str_replace('\\','',$_COOKIE[$config['cookies.name']]));
+
+                // Lookup the users table for that user
+                $db->where("username", validate_input($remarray['username']));
+                $db->where("user_id", validate_input($remarray['uid']));
+                $db->where("remember", validate_input($remarray['rem']));
+                $user_info = $db->get("users", 1);
+
+                // The submitted details are valid
+                if(isset($user_info[0])) {
+                    $_SESSION['hn_login']['id'] = $user_info[0];
+                    $_SESSION['hn_login']['name'] = $remarray['username'];
+                    $_SESSION['hn_login']['comm'] = $user_info[2];
+
+                    header('Location: index.php');
+                    exit;
+                }
+            }
+        }
+    }
 
     static function page_nav($total,$page,$perpage,$url) {
         $page_arr = array();
@@ -87,7 +86,7 @@ class Common {
         }
 
         if ($page>1) {
-            $page_arr[$arr_count]['title'] = "Prev";
+            $page_arr[$arr_count]['title'] = "上一页";
             $page_arr[$arr_count]['link'] = $url.'page='. ($page-1);
             $page_arr[$arr_count]['current'] = 0;
 
@@ -119,17 +118,17 @@ class Common {
         return $page_arr;
     }
 
-   static function getrandnum($length) {
-      $randstr='';
-      srand((double)microtime()*1000000);
-      $chars = array ( 'a','b','C','D','e','f','G','h','i','J','k','L','m','N','P','Q','r','s','t','U','V','W','X','y','z','1','2','3','4','5','6','7','8','9');
-      for ($rand = 0; $rand <= $length; $rand++) {
-         $random = rand(0, count($chars) -1);
-         $randstr .= $chars[$random];
-      }
+    static function getrandnum($length) {
+        $randstr='';
+        srand((double)microtime()*1000000);
+        $chars = array ( 'a','b','C','D','e','f','G','h','i','J','k','L','m','N','P','Q','r','s','t','U','V','W','X','y','z','1','2','3','4','5','6','7','8','9');
+        for ($rand = 0; $rand <= $length; $rand++) {
+            $random = rand(0, count($chars) -1);
+            $randstr .= $chars[$random];
+        }
 
-      return $randstr;
-   }
+        return $randstr;
+    }
 
     static function time_taken($time) {
         if($time > 86400) {
@@ -171,49 +170,45 @@ class Common {
         return $x;
     }
 
-   static function getDomain($url) {
-      $parts = parse_url($url);
+    static function getDomain($url) {
+        $parts = parse_url($url);
 
-      if(isset($parts['host'])) {
-         $parts['host'] = str_replace('www.','',$parts['host']);
+        if(isset($parts['host'])) {
+            $parts['host'] = str_replace('www.','',$parts['host']);
+            return $parts['host'];
+        } else {
+            $url = str_replace('www.','',$url);
+            return $url;
+        }
+    }
 
-         return $parts['host'];
-      } else {
-         $url = str_replace('www.','',$url);
+    static function validate_input($input,$dbcon=false,$content='all',$maxchars=0) {
+        if(get_magic_quotes_gpc()) {
+            if(ini_get('magic_quotes_sybase')) {
+                $input = str_replace("''", "'", $input);
+            } else {
+                $input = stripslashes($input);
+            }
+        }
 
-         return $url;
-      }
-   }
+        if($content == 'alnum') {
+            $input = ereg_replace("[^a-zA-Z0-9]", '', $input);
+        } elseif($content == 'num') {
+            $input = ereg_replace("[^0-9]", '', $input);
+        } elseif($content == 'alpha') {
+            $input = ereg_replace("[^a-zA-Z]", '', $input);
+        }
 
-   static function validate_input($input,$dbcon=false,$content='all',$maxchars=0) {
-      if(get_magic_quotes_gpc()) {
-         if(ini_get('magic_quotes_sybase')) {
-            $input = str_replace("''", "'", $input);
-         } else {
-            $input = stripslashes($input);
-         }
-      }
+        if($maxchars) {
+            $input = substr($input,0,$maxchars);
+        }
 
-      if($content == 'alnum') {
-         $input = ereg_replace("[^a-zA-Z0-9]", '', $input);
-      } elseif($content == 'num') {
-         $input = ereg_replace("[^0-9]", '', $input);
-      } elseif($content == 'alpha') {
-         $input = ereg_replace("[^a-zA-Z]", '', $input);
-      }
+        if($dbcon) {
+            $input = mysql_real_escape_string($input);
+        } else {
+            $input = mysql_escape_string($input);
+        }
 
-      if($maxchars) {
-         $input = substr($input,0,$maxchars);
-      }
-
-      if($dbcon) {
-         $input = mysql_real_escape_string($input);
-      } else {
-         $input = mysql_escape_string($input);
-      }
-
-      return $input;
-   }
+        return $input;
+    }
 }
-
-?>
